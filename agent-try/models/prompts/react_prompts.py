7921b51f4f5a681e402d3b653764b64d220698ca
ganzhiyu <syu015201@163.com>
2026-03-12 14:52:50 +0800

    React-fixed

--- agent-try/models/prompts/react_prompts.py
+++ agent-try/models/prompts/react_prompts.py
@@ -2,7 +2,7 @@
 Author: ganzhiyu syu015201@163.com
 Date: 2026-03-12 11:32:47
 LastEditors: ganzhiyu syu015201@163.com
-LastEditTime: 2026-03-12 14:20:57
+LastEditTime: 2026-03-12 14:50:41
 FilePath: \Agents\agent-try\prompts\react_prompts.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
